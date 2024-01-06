296f992e9039f28ba36b94e1ea3f31896e3d15b2
Zach <zanderson004@gmail.com>
2024-01-06 15:31:41 +1030

    from mac

---
+++ src/components/SudokuBoard.vue
@@ -0,0 +1,31 @@
+<!-- SudokuBoard.vue -->
+
+<script lang="ts">
+  import SudokuCell from './SudokuCell.vue';
+
+  export default {
+    name: 'SudokuBoard',
+    components: {
+      SudokuCell
+    }
+    // Add data, methods, or other component options here
+  }
+</script>
+
+<template>
+  <div class="sudoku-board">
+    <div v-for="row in 9" class="sudoku-row">
+      <SudokuCell v-for="col in 9" :coords="String(row) + String(col)" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .sudoku-row {
+    display: block;
+  }
+
+  [coords="22"] {
+    
+  }
+</style>
---
+++ src/components/SudokuCell.vue
@@ -0,0 +1,26 @@
+<!-- SudokuCell.vue -->
+
+<script lang="ts">
+  export default {
+    name: 'SudokuCell',
+    // Add data, methods, or other component options here
+  }
+  let val = -1;
+</script>
+
+<template>
+    <div class="sudoku-cell">
+      -1
+    </div>
+</template>
+  
+<style scoped>
+  .sudoku-cell {
+    width: 40px;
+    height: 40px;
+    border: 1px solid #ffffff;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
